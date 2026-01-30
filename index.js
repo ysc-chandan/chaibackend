e77acf22118dce164a38e1cb8080742315a6ef27
Chandan Kumar <chandank72572@gmail.com>
2026-01-30 23:38:46 +0530

    github data added

--- index.js
+++ index.js
@@ -22,6 +22,10 @@ app.get('/youtube', (req,res)=>{
     res.send("<h2>Chai or Code</h2>")
 })
 
+app.get('/github', (req,res)=>{
+    res.json(githubData)
+})
+
 app.listen(process.env.PORT, () => {
         console.log(`Example app listening on port ${port}`);
 });
